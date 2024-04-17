bfc8439f182b9119edb0901ae8ad80cd3eb2c187
AnshumanNeon <anshumanneon@gmail.com>
2024-04-17 15:58:09 +0530

    default image

--- include/test.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void print(char* a) {
-  printf("%s", a);
-}
--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
-#include "test.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  print("a");
+  FILE* file = fopen("./image.ppm", "w+");
+
+  int img_width = 256, img_height = 256;
+  
+  fprintf(file, "P3\n%d %d\n255\n", img_width, img_height);
+
+  for(int i = 0; i < img_height; i++) {
+    for(int j = 0; j < img_width; j++) {
+      double r = (double)i / (img_width-1);
+      double g = (double)j / (img_height-1);
+      double b = 0;
+
+      int ir = (int)(255.99 * r);
+      int ig = (int)(255.999 * g);
+      int ib = (int)(255.999 * b);
+
+      fprintf(file, "%d %d %d\n", ir, ig, ib);
+    }
+  }
   return 0;
 }

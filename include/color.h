ad4778f0021e8b65b84db4375740b9664945c8b2
AnshumanNeon <anshumanneon@gmail.com>
2024-04-19 18:07:24 +0530

    a red sphere

--- include/color.h
+++ include/color.h
@@ -17,9 +17,16 @@ HMM_Vec3 unit_dir(HMM_Vec3* vec) {
 }
 
 HMM_Vec3 ray_color(Ray* ray) {
+  HMM_Vec3 test_sphere_center = { .X = 0, .Y = 0, .Z = -1 };
+  
+  if(hit_sphere(&test_sphere_center, 0.5, ray) == true) {
+    HMM_Vec3 new_color = { .R = 1, .G = 0, .B = 0 };
+    return new_color;
+  }
+
   HMM_Vec3 unit = unit_dir(&ray->dir);
   float a = 0.5*(unit.Y + 1);
-  HMM_Vec3 b = { .X = 1, .Y = 1, .Z = 1 };
+  HMM_Vec3 b = { .X = 1.0, .Y = 1.0, .Z = 1 };
   HMM_Vec3 c = { .X = 0.5, .Y = 0.7, .Z = 1 };
   return HMM_AddV3(HMM_MulV3F(b, 1-a), HMM_MulV3F(c, a));
 }
--- include/ray.h
+++ include/ray.h
@@ -1,6 +1,7 @@
 #ifndef RAY_H
 #define RAY_H
 
+#include <stdbool.h>
 #include "../deps/handmademath/HandmadeMath.h"
 
 typedef struct Ray {
@@ -10,6 +11,16 @@ typedef struct Ray {
 
 HMM_Vec3 ray_at(Ray* ray, float t) {
   return HMM_AddV3(ray->origin, HMM_MulV3F(ray->dir, t));
+}
+
+bool hit_sphere(HMM_Vec3* center, float radius, Ray* ray) {
+  HMM_Vec3 oc = HMM_SubV3(*center, ray->origin);
+  float a = HMM_DotV3(ray->dir, ray->dir);
+  float b = -2.0 * HMM_DotV3(ray->dir, oc);
+  float c = HMM_DotV3(oc, oc) - (radius*radius);
+  float discriminant = b*b - 4*a*c;
+
+  return (discriminant >= 0);
 }
 
 #endif

b24db4d3a689cfb64700d5347d683ccc73dccb25
AnshumanNeon <anshumanneon@gmail.com>
2024-04-20 14:25:01 +0530

    added intervals, need to implement

---
+++ include/interval.h
@@ -0,0 +1,24 @@
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+#include <cmath>
+typedef struct Interval {
+  float min, max;
+} Interval;
+
+float interval_size(Interval* interval) {
+  return interval->max - interval->min;
+}
+
+bool contains(Interval* interval, float x) {
+  return x <= interval->max && x >= interval->min;
+}
+
+bool surrounds(Interval* interval, float x) {
+  return x < interval->max && x > interval->min;
+}
+
+const Interval empty = { .min = INFINITY, .max = -INFINITY };
+const Interval universe = { .min = -INFINITY, .max = INFINITY };
+
+#endif
